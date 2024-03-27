bbe57ccb8c671521d130a7277cee6085d4fb9469
Vlad <vlad.fandikov@gmai.com>
2024-03-27 17:01:14 +0200

    27.03

--- mysite2/blog/urls.py
+++ mysite2/blog/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('post-add', views.post_add, name='post_add',)
+    path('post-add', views.post_add, name='post_add',),
+    path('post-edit/<int:post_id>/',views.post_edit, name='post_edit'),
 ]
--- mysite2/blog/views.py
+++ mysite2/blog/views.py
@@ -14,6 +14,19 @@ from django.contrib.auth.decorators import login_required
 
 
 from .forms import PostForm
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post_edit_form = PostForm(instance=post)
+    if request.method == 'POST':
+        post_edit_form = PostForm(request.POST, request.FILES, instance=post)
+        if post_edit_form.is_valid():
+            post_edit_form.save()
+    return render(request,
+                  'blog/account/post_edit.html',
+                  {'form': post_edit_form,
+                   'post': post})
 @login_required
 def post_add(request):
     user=request.user
@@ -24,6 +37,8 @@ def post_add(request):
             post.author=user
             print(post)
             post.save()
+            for tag in form.cleaned_data['tags']:
+                post.tags.add(tag)
     else:
         form=PostForm()
 
